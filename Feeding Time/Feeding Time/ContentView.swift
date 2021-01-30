6a1d4c2583a15ccbf887a31cf1d21f2ac3ddac56
blukasi <nerdy_brent@hotmail.com>
2021-01-30 15:28:34 -0500

    v1.0.0
    initial commit for the feeding time project files

---
+++ Feeding Time/Feeding Time/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Feeding Time
+//
+//  Created by Brent Lukasik on 2021-01-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Feeding Time/Feeding Time/Feeding_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feeding_TimeApp.swift
+//  Feeding Time
+//
+//  Created by Brent Lukasik on 2021-01-30.
+//
+
+import SwiftUI
+
+@main
+struct Feeding_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
